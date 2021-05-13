5ffcaee3098a4fac5cc1ef060f2d63731037cb89
iainbest <iainbest@users.noreply.github.com>
2021-05-13 14:22:05 +0000

    deploy: HetSys/PX915_2021_A@2a99cee52263fbd8cf249a5823c5691d9dc47eba

--- fortran/docs/namespaces_dup.js
+++ fortran/docs/namespaces_dup.js
@@ -63,12 +63,12 @@ var namespaces_dup =
     ] ],
     [ "write_netcdf", "namespacewrite__netcdf.html", [
       [ "error_check", "namespacewrite__netcdf.html#aca2bb13e88c9dbfc7d0027b1bd83d9d0", null ],
-      [ "write_qho_main", "namespacewrite__netcdf.html#ae2722e18129ba78f02541fa0ae49f886", null ],
       [ "write_qho_equilibration", "namespacewrite__netcdf.html#a4b9b251a06719d1b0470cfc1f03883ab", null ],
-      [ "write_h2plus_main", "namespacewrite__netcdf.html#a818c3a109a649bbf802adaa2a937a458", null ],
       [ "write_h2plus_equilibration", "namespacewrite__netcdf.html#a45c5f8affdc7b8d275bc35f8b70337f4", null ],
-      [ "write_h2_main", "namespacewrite__netcdf.html#a8b125766a34e4cc2d72228a5bd109ce0", null ],
       [ "write_h2_equilibration", "namespacewrite__netcdf.html#adc83694755df0f3d9228fb9aa0874452", null ],
+      [ "write_qho_main", "namespacewrite__netcdf.html#ae2722e18129ba78f02541fa0ae49f886", null ],
+      [ "write_h2plus_main", "namespacewrite__netcdf.html#a818c3a109a649bbf802adaa2a937a458", null ],
+      [ "write_h2_main", "namespacewrite__netcdf.html#a8b125766a34e4cc2d72228a5bd109ce0", null ],
       [ "append_to_array", "namespacewrite__netcdf.html#a6830bd805591aad1ec622d12037dd99d", null ]
     ] ]
 ];
--- fortran/docs/netcdf__out_8f90.js
+++ fortran/docs/netcdf__out_8f90.js
@@ -1,11 +1,11 @@
 var netcdf__out_8f90 =
 [
     [ "error_check", "netcdf__out_8f90.html#aca2bb13e88c9dbfc7d0027b1bd83d9d0", null ],
-    [ "write_qho_main", "netcdf__out_8f90.html#ae2722e18129ba78f02541fa0ae49f886", null ],
     [ "write_qho_equilibration", "netcdf__out_8f90.html#a4b9b251a06719d1b0470cfc1f03883ab", null ],
-    [ "write_h2plus_main", "netcdf__out_8f90.html#a818c3a109a649bbf802adaa2a937a458", null ],
     [ "write_h2plus_equilibration", "netcdf__out_8f90.html#a45c5f8affdc7b8d275bc35f8b70337f4", null ],
-    [ "write_h2_main", "netcdf__out_8f90.html#a8b125766a34e4cc2d72228a5bd109ce0", null ],
     [ "write_h2_equilibration", "netcdf__out_8f90.html#adc83694755df0f3d9228fb9aa0874452", null ],
+    [ "write_qho_main", "netcdf__out_8f90.html#ae2722e18129ba78f02541fa0ae49f886", null ],
+    [ "write_h2plus_main", "netcdf__out_8f90.html#a818c3a109a649bbf802adaa2a937a458", null ],
+    [ "write_h2_main", "netcdf__out_8f90.html#a8b125766a34e4cc2d72228a5bd109ce0", null ],
     [ "append_to_array", "netcdf__out_8f90.html#a6830bd805591aad1ec622d12037dd99d", null ]
 ];
