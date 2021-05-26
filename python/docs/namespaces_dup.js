7d80edd1f40fca3c0e76e9e3f8fed5d5958a3b35
joegilkes <joegilkes@users.noreply.github.com>
2021-05-26 18:32:05 +0000

    deploy: HetSys/PX915_2021_A@a0979e7d06ea31ac8d5ebddedf0037791c2a7d23

--- python/docs/namespaces_dup.js
+++ python/docs/namespaces_dup.js
@@ -1,9 +1,4 @@
 var namespaces_dup =
 [
-    [ "dice_check", "namespacedice__check.html", null ],
-    [ "dice_defaults", "namespacedice__defaults.html", null ],
-    [ "dice_inputs", "namespacedice__inputs.html", null ],
-    [ "dice_visualise", "namespacedice__visualise.html", null ],
-    [ "dice_write", "namespacedice__write.html", null ],
     [ "DicePy", "namespace_dice_py.html", "namespace_dice_py" ]
 ];
--- python/docs/search/namespaces_0.js
+++ python/docs/search/namespaces_0.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['dice_5fcheck_82',['dice_check',['../namespacedice__check.html',1,'dice_check'],['../namespace_dice_py_1_1dice__check.html',1,'DicePy.dice_check']]],
-  ['dice_5fdefaults_83',['dice_defaults',['../namespacedice__defaults.html',1,'dice_defaults'],['../namespace_dice_py_1_1dice__defaults.html',1,'DicePy.dice_defaults']]],
-  ['dice_5finputs_84',['dice_inputs',['../namespacedice__inputs.html',1,'dice_inputs'],['../namespace_dice_py_1_1dice__inputs.html',1,'DicePy.dice_inputs']]],
-  ['dice_5fvisualise_85',['dice_visualise',['../namespacedice__visualise.html',1,'dice_visualise'],['../namespace_dice_py_1_1dice__visualise.html',1,'DicePy.dice_visualise']]],
-  ['dice_5fwrite_86',['dice_write',['../namespacedice__write.html',1,'dice_write'],['../namespace_dice_py_1_1dice__write.html',1,'DicePy.dice_write']]],
+  ['dice_5fcheck_82',['dice_check',['../namespace_dice_py_1_1dice__check.html',1,'DicePy']]],
+  ['dice_5fdefaults_83',['dice_defaults',['../namespace_dice_py_1_1dice__defaults.html',1,'DicePy']]],
+  ['dice_5finputs_84',['dice_inputs',['../namespace_dice_py_1_1dice__inputs.html',1,'DicePy']]],
+  ['dice_5fvisualise_85',['dice_visualise',['../namespace_dice_py_1_1dice__visualise.html',1,'DicePy']]],
+  ['dice_5fwrite_86',['dice_write',['../namespace_dice_py_1_1dice__write.html',1,'DicePy']]],
   ['dicepy_87',['DicePy',['../namespace_dice_py.html',1,'']]]
 ];
